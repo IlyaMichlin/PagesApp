cbf1d22e5e7d5e9a4a91575804c53e84fe13f6a5
Ilya <ilyamichlin1@gmail.com>
2021-04-09 20:07:17 +0300

    Updated SECRET_KEY

--- PagesApp/settings.py
+++ PagesApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xlF&kzOoKvfB^LsH@mw5PQQQ9TXtngYWvgiMMN72yAmtJko0kh'
+SECRET_KEY = 'django-insecure-P^2MfEcvgEbHio7GQ1jNu&oH9X^ybYy85ree~9h11u(@A8uxF3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
